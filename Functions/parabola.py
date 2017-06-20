90954b3d3938ed26ee99a9b84d7bd6c45a9a5b9f
Henrique Junior <henriquecsj@gmail.com>
2017-06-20 19:23:58 -0300

    20/6
    20/6

---
+++ Functions/parabola.py
@@ -0,0 +1,91 @@
+# import tkinter
+
+
+# def parabola(x):
+#     y = (x * x)/100
+#     return y
+
+
+# def draw_axes(page):
+#     page.update()
+#     x_origin = page.winfo_width() / 2
+#     y_origin = page.winfo_height() / 2
+#     page.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
+#     page.create_line(-x_origin, 0, x_origin, 0, fill="white")
+#     page.create_line(0, y_origin, 0, -y_origin, fill="white")
+
+
+# def plot(canvas, x, y):
+#     canvas.create_line(x, y, x + 1, y + 1, fill="red")
+
+# mainWindow = tkinter.Tk()
+# mainWindow.title("Parabola")
+# mainWindow.geometry("640x480")
+
+# canvas = tkinter.Canvas(mainWindow, width=640, height=480)
+# canvas.grid(row=0, column=0)
+
+# draw_axes(canvas)
+
+
+# for x in range(-100, 100):
+#     y = parabola(x)
+#     plot(canvas, x, -y)
+
+# mainWindow.mainloop()
+
+import tkinter
+import math
+
+
+def parabola(page, size):
+    # for x in range(-size, size):
+    for x in range(size):
+        y = (x * x)/size
+        plot(page, x, y)
+        plot(page, -x, y)
+
+
+def circle(page, radius, g, h):
+    page.create_oval(g + radius, h + radius, g - radius, h - radius, outline="red", width=2)
+
+    # for x in range(g * 100, (g + radius) * 100):
+    #     x /= 100
+    #     print(x)
+    #     y = h + (math.sqrt(radius ** 2 - ((x - g) ** 2)))
+    #     plot(page, x, y)
+    #     plot(page, x, 2 * h - y)
+    #     plot(page, 2 * g - x, y)
+    #     plot(page, 2 * g - x, 2 * h - y)
+
+
+def draw_axes(page):
+    page.update()
+    x_origin = page.winfo_width() / 2
+    y_origin = page.winfo_height() / 2
+    page.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
+    page.create_line(-x_origin, 0, x_origin, 0, fill="black")
+    page.create_line(0, y_origin, 0, -y_origin, fill="black")
+
+
+def plot(page, x, y):
+    page.create_line(x, -y, x + 1, -y + 1, fill="red")
+
+mainWindow = tkinter.Tk()
+mainWindow.title("Parabola")
+mainWindow.geometry("640x480")
+
+canvas = tkinter.Canvas(mainWindow, width=640, height=480)
+canvas.grid(row=0, column=0)
+
+draw_axes(canvas)
+
+parabola(canvas, 100)
+parabola(canvas, 200)
+
+circle(canvas, 100, 100, 100)
+circle(canvas, 100, 100, -100)
+circle(canvas, 100, -100, -100)
+
+
+mainWindow.mainloop()
