5c1353c8147c307daee8dd03c8fff804d51e2234
Henrique Junior <henriquecsj@gmail.com>
2017-06-26 11:09:30 -0300

    26/6

--- OOP/inheritance.py
+++ OOP/inheritance.py
@@ -19,16 +19,50 @@ class Employee:
 class Developer(Employee):
     raise_amt = 1.10
 
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
-dev_1 = Employee('Corey', 'Schafer', 50000)
-dev_2 = Developer('Test', 'Employee', 60000)
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_empl(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print("-->", emp.fullname())
+
+
+dev_1 = Developer('Corey', 'Schafer', 50000, "Python")
+dev_2 = Developer('Test', 'Employee', 60000, "Java")
+mgr_1 = Manager("Sue", " Smith", 90000, [dev_1])
+
+print(mgr_1.email)
+# mgr_1.print_emps()
+mgr_1.remove_emp(dev_1)
+mgr_1.add_empl(dev_2)
+mgr_1.print_emps()
+
+print(isinstance(mgr_1, Developer))
 
 # print(help(Developer))
 
 
-print(dev_1.pay)
-dev_1.apply_raise()
-print(dev_1.pay)
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.prog_lang)
 
 
 # print(dev_1.raise_amt)
