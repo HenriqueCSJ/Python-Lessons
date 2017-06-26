0f698cb969b9af173013f972ae5d4990e1e0f3c9
Henrique Junior <henriquecsj@gmail.com>
2017-06-25 21:49:54 -0300

    Teste

--- OOP/inheritance.py
+++ OOP/inheritance.py
@@ -16,11 +16,19 @@ class Employee:
         self.pay = int(self.pay * self.raise_amt)
 
 
-dev_1 = Employee('Corey', 'Schafer', 50000)
-dev_2 = Employee('Test', 'Employee', 60000)
+class Developer(Employee):
+    raise_amt = 1.10
 
-print(dev_1.email)
-print(dev_2.email)
+
+dev_1 = Employee('Corey', 'Schafer', 50000)
+dev_2 = Developer('Test', 'Employee', 60000)
+
+# print(help(Developer))
+
+
+print(dev_1.pay)
+dev_1.apply_raise()
+print(dev_1.pay)
 
 
 # print(dev_1.raise_amt)
