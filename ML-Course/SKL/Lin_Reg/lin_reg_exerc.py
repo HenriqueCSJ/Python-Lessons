461c66227cf8062454c6d9c1c84dfeb9f8e19fea
Henrique Junior <henriquecsj@gmail.com>
2019-01-05 19:44:38 -0200

    Missing Files
    Some missing files added

--- ML-Course/SKL/Lin_Reg/lin_reg_exerc.py
+++ ML-Course/SKL/Lin_Reg/lin_reg_exerc.py
@@ -1,9 +1,10 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-customers = pd.read_csv("Ecommerce Customers")
-# print(customers)
-print(customers.columns)
-print(customers.info())
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+
